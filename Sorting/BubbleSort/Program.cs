1d6a1b9cf714530018a7f48299a8c4b0c6b59365
Yash Gupta <yash.gupta3108@gmail.com>
2023-01-10 21:39:11 +0530

    Added code for Selection Sort & Bubble Sort

---
+++ Sorting/BubbleSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new int[20];
+
+            Console.Write("Enter the number of elements : ");
+            var size = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < size; i++)
+            {
+                Console.Write($"Enter element {(i + 1)} : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Sort(arr, size);
+
+            Console.WriteLine("Sorted array is : ");
+            for (var i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Sort(int[] arr, int n)
+        {
+            int swaps;
+
+            for (var i = n - 2; i >= 0; i--)
+            {
+                swaps = 0;
+
+                for (var j = 0; j <= i; j++)
+                {
+                    if (arr[j] > arr[j + 1]) //// outer loop runs until second last element to avoid IndexOutOfBound exception at this condition
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swaps++;
+                    }
+                }
+
+                if (swaps == 0) //// no swaps in a particular pass denotes that the array has been sorted
+                    break;
+            }
+        }
+    }
+}
---
+++ Sorting/SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new int[20];
+
+            Console.Write("Enter the number of elements : ");
+            var size = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < size; i++)
+            {
+                Console.Write($"Enter element {(i + 1)} : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            SelectionSort(arr, size);
+
+            Console.WriteLine("Sorted array is : ");
+            for (var i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void SelectionSort(int[] arr, int n)
+        {
+            int minIndex;
+
+            for (var i = 0; i < n - 1; i++)
+            {
+                minIndex = i;
+
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+                }
+
+                if (i != minIndex)
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+    }
+}
