1073c313735c60ca1395b34fe1fe9b95ffded9af
Yash Gupta <yash.gupta3108@gmail.com>
2023-05-26 19:30:44 +0530

    Added code for Binary Search

---
+++ Searching/BinarySearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new int[100];
+
+            Console.Write("Enter the number of elements : ");
+            var size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the elements in sorted order: ");
+            for (var i = 0; i < size; i++)
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the search value : ");
+            var searchValue = Convert.ToInt32(Console.ReadLine());
+
+            var index = BinarySearch(arr, size, searchValue);
+
+            if (index == -1)
+                Console.WriteLine($"Value {searchValue} not present in the array.");
+            else
+                Console.WriteLine($"Value {searchValue} present at index: {index}");
+        }
+
+        internal static int BinarySearch(int[] arr, int size, int searchValue)
+        {
+            int first = 0, last = size - 1;
+
+            while (first <= last)
+            {
+                var mid = (first + last) / 2;
+
+                if (searchValue < arr[mid])
+                    last = mid - 1;         //// Search in left half
+                else if (searchValue > arr[mid])
+                    first = mid + 1;        //// Search in right half
+                else
+                    return mid;             //// searchValue present at index mid
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Searching/BinarySearchRecursive/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BinarySearchRecursive
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new int[100];
+
+            Console.Write("Enter the number of elements : ");
+            var size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the elements in sorted order: ");
+            for (var i = 0; i < size; i++)
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the search value : ");
+            var searchValue = Convert.ToInt32(Console.ReadLine());
+
+            var index = BinarySearchRecursive(arr, size, searchValue);
+
+            if (index == -1)
+                Console.WriteLine($"Value {searchValue} not present in the array.");
+            else
+                Console.WriteLine($"Value {searchValue} present at index: {index}");
+        }
+
+        internal static int BinarySearchRecursive(int[] arr, int size, int searchValue)
+        {
+            return Search(arr, 0, size - 1, searchValue);
+        }
+
+        private static int Search(int[] arr, int first, int last, int searchValue)
+        {
+            if (first > last)
+                return -1;
+
+            var mid = (first + last) / 2;
+
+            if (searchValue > arr[mid])           //// Search in right half
+                return Search(arr, mid + 1, last, searchValue);
+            else if (searchValue < arr[mid])      //// Search in left half
+                return Search(arr, first, mid - 1, searchValue);
+            else
+                return mid;
+        }
+    }
+}
